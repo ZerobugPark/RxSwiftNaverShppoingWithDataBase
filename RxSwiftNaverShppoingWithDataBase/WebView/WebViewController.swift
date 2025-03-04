72cc2a3d7c77da205d18ff0d459456548699f781
young <zerobugpark@gmail.com>
2025-03-04 23:54:19 +0900

    [기능추가] - activityIndicator

--- RxSwiftNaverShppoingWithDataBase/WebView/WebViewController.swift
+++ RxSwiftNaverShppoingWithDataBase/WebView/WebViewController.swift
@@ -13,13 +13,14 @@ import SnapKit
 final class WebViewController: UIViewController {
 
     private let web = WKWebView()
+    private let activityIndicator = UIActivityIndicatorView()
     
     var url: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        print(url)
+        activityIndicator.startAnimating()
         layout()
         loadWebPage(url)
         web.navigationDelegate = self
@@ -37,9 +38,13 @@ final class WebViewController: UIViewController {
     
     private func layout() {
         view.addSubview(web)
+        view.addSubview(activityIndicator)
         
         web.snp.makeConstraints { make in
             make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+        activityIndicator.snp.makeConstraints { make in
+            make.center.equalToSuperview()
         }
     }
 
@@ -48,5 +53,20 @@ final class WebViewController: UIViewController {
 }
 
 extension WebViewController: WKNavigationDelegate {
+    // 로딩 중
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        
+    }
     
+    //로딩 완료
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        activityIndicator.stopAnimating()
+        activityIndicator.isHidden = true
+    }
+    
+    //로딩 실패
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: any Error) {
+        activityIndicator.stopAnimating()
+        activityIndicator.isHidden = true
+    }
 }
